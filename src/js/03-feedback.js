706b10690fcafa858945d8bacd2ebdbfe265321d
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2022-11-19 23:03:17 +0200

    o

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import { throttle } from '@vimeo/player';
+import { throttle } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
